052e6346b1f7703a12a894cef915c021f982cb3f
roman.tolok1 <romantolok@ukr.net>
2020-04-05 17:12:23 +0300

    added seamless texture option

--- Assets/Scripts/TextureCreatorWindow.cs
+++ Assets/Scripts/TextureCreatorWindow.cs
@@ -1,25 +1,23 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
+
 [ExecuteInEditMode]
 public class TextureCreatorWindow : EditorWindow
 {
+    private bool alphaToggle;
     private string filename = "MyProceduralTexture";
-    private float perlinXScale;
-    private float perlinYScale;
-    private int perlinOctaves;
-    private float perlinPersistance;
+    private bool mapToggle;
     private float perlinHeightScale;
+    private int perlinOctaves;
     private int perlinOffsetX;
     private int perlinOffsetY;
-    private bool alphaToggle = false;
-    private bool seamlessToggle = false;
-    private bool mapToggle = false;
+    private float perlinPersistance;
+    private float perlinXScale;
+    private float perlinYScale;
 
     private Texture2D pTexture2D;
-    
+    private bool seamlessToggle;
+
     [MenuItem("Window/TextureCreatorWindow")]
     public static void ShowWindow()
     {
@@ -28,7 +26,7 @@ public class TextureCreatorWindow : EditorWindow
 
     private void OnEnable()
     {
-        pTexture2D = new Texture2D(513,513,TextureFormat.ARGB32,false);
+        pTexture2D = new Texture2D(513, 513, TextureFormat.ARGB32, false);
     }
 
     private void OnGUI()
@@ -37,7 +35,7 @@ public class TextureCreatorWindow : EditorWindow
         filename = EditorGUILayout.TextField("Texture Name", filename);
 
         int wSize = (int) (EditorGUIUtility.currentViewWidth - 100);
-        
+
         perlinXScale = EditorGUILayout.Slider("X Scale", perlinXScale, 0, 0.1f);
         perlinYScale = EditorGUILayout.Slider("Y Scale", perlinYScale, 0, 0.1f);
         perlinOctaves = EditorGUILayout.IntSlider("Octaves", perlinOctaves, 1, 10);
@@ -48,7 +46,7 @@ public class TextureCreatorWindow : EditorWindow
         alphaToggle = EditorGUILayout.Toggle("Alpha?", alphaToggle);
         mapToggle = EditorGUILayout.Toggle("Map?", mapToggle);
         seamlessToggle = EditorGUILayout.Toggle("Seamless", seamlessToggle);
-        
+
         GUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
 
@@ -61,31 +59,71 @@ public class TextureCreatorWindow : EditorWindow
             {
                 for (int x = 0; x < size; x++)
                 {
-                    pValue = Utils.FractalBrownianMotion((x + perlinOffsetX) * perlinXScale,
-                        (y + perlinOffsetY) * perlinYScale, perlinOctaves,
-                        perlinPersistance) * perlinHeightScale;
+                    if (seamlessToggle)
+                    {
+                        float u = x / (float) size;
+                        float v = y / (float) size;
+
+                        float noise00 = Utils.FractalBrownianMotion((x + perlinOffsetX) * perlinXScale,
+                            (y + perlinOffsetY) * perlinYScale,
+                            perlinOctaves,
+                            perlinPersistance) * perlinHeightScale;
+                        float noise01 = Utils.FractalBrownianMotion((x + perlinOffsetX) * perlinXScale,
+                            (y + perlinOffsetY + size) * perlinYScale,
+                            perlinOctaves,
+                            perlinPersistance) * perlinHeightScale;
+                        float noise10 = Utils.FractalBrownianMotion((x + perlinOffsetX + size) * perlinXScale,
+                            (y + perlinOffsetY) * perlinYScale,
+                            perlinOctaves,
+                            perlinPersistance) * perlinHeightScale;
+                        float noise11 = Utils.FractalBrownianMotion((x + perlinOffsetX + size) * perlinXScale,
+                            (y + perlinOffsetY + size) * perlinYScale,
+                            perlinOctaves,
+                            perlinPersistance) * perlinHeightScale;
+
+                        float noiseTotal = u * v * noise00 + u * (1 - v) * noise01 +
+                                           (1 - u) * v * noise10 +
+                                           (1 - u) * (1 - v) * noise11;
+
+                        int offset = 50;
+                        float value = (int) (256 * noiseTotal) + offset;
+                        float r = Mathf.Clamp((int) noise00, 0, 255);
+                        float g = Mathf.Clamp(value, 0, 255);
+                        float b = Mathf.Clamp(value + offset, 0, 255);
+                        float a = Mathf.Clamp(value + offset * 2, 0, 255);
+
+                        pValue = (r + g + b) / (3 * 255.0f);
+                    }
+                    else
+                    {
+                        pValue = Utils.FractalBrownianMotion((x + perlinOffsetX) * perlinXScale,
+                            (y + perlinOffsetY) * perlinYScale, perlinOctaves,
+                            perlinPersistance) * perlinHeightScale;
+                    }
+
+                    
                     float colValue = pValue;
                     pixColor = new Color(colValue, colValue, colValue, alphaToggle ? colValue : 1);
-                    pTexture2D.SetPixel(x,y,pixColor);
+                    pTexture2D.SetPixel(x, y, pixColor);
                 }
             }
-            pTexture2D.Apply(false,false);
+
+            pTexture2D.Apply(false, false);
         }
-        
+
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
-        
+
         GUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
         GUILayout.Label(pTexture2D, GUILayout.Width(wSize), GUILayout.Height(wSize));
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
-        
+
         GUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
         if (GUILayout.Button("Save", GUILayout.Width(wSize)))
         {
-            
         }
     }
 }
